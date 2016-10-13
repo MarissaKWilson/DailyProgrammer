a3b78433f0df0cdf9c376ec7126a782e186cc1d3
Marissa Wilson <Marissa@MarissaKWilson.com>
2016-10-13 13:45:21 -0400

    Dictionary updated Logic implemented for translation functions Testing pending “get” issue

--- Java/MorseCodeTranslator/src/translator.java
+++ Java/MorseCodeTranslator/src/translator.java
@@ -1,4 +1,45 @@
+import java.util.Scanner;
 
 public class translator {
-
+	public static void Main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Enter text or morse code to be translated: ");
+		String str = scan.nextLine();
+		String[] phrase = str.split("\\s");
+		String translation = "Unable to translate";
+		if (str.startsWith(".") || str.startsWith("-")){
+			translation = TranslateFromMorse(phrase);
+		} else{
+			translation = TranslateFromEnglish(phrase);
+		}
+		
+	}
+	
+	public static String TranslateFromMorse(String[] phrase){
+		MorseDictionary morsedict = new MorseDictionary('c');
+		String translation = "";
+		for (int i = 0; i > phrase.length; i++){
+			String key = phrase[i];
+			if (key.startsWith(" ")){
+				translation.concat(" ");
+				key = key.trim();
+			}
+			String value = morsedict.get(key);
+			translation.concat(morsedict.get(key));
+		}	
+		return translation;
+	}
+	
+	public static String TranslateFromEnglish(String[] phrase){
+		MorseDictionary morsedict = new MorseDictionary("S");
+		String translation = "";
+		for (int i = 0; i > phrase.length; i++){
+			String key = phrase[i].toUpperCase();
+			String value = morsedict.get(key);
+			translation.concat(morsedict.get(key));
+			translation.concat(" ");
+		}
+		return translation;
+	}
 }
